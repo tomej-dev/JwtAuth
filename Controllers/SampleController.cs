be4f1fcbc77e11b232c40c7a55b98778dc19465c
tomej-dev <joaocarlos8221@gmail.com>
2025-10-29 14:43:58 -0300

    finished project

--- Controllers/SampleController.cs
+++ Controllers/SampleController.cs
@@ -1,63 +1,24 @@
-using System.Text;
-using JwtAuth.Data;
-using JwtAuth.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace JwtAuthSQLite.Controllers;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Banco SQLite
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Serviço de usuários
-builder.Services.AddScoped<UserService>();
-
-// Configuração JWT
-var jwt = builder.Configuration.GetSection("Jwt");
-var key = jwt["Key"];
-var issuer = jwt["Issuer"];
-var audience = jwt["Audience"];
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = issuer,
-            ValidateAudience = true,
-            ValidAudience = audience,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!))
-        };
-    });
-
-builder.Services.AddAuthorization();
-
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-// Cria o banco automaticamente (somente em desenvolvimento)
-using (var scope = app.Services.CreateScope())
+[ApiController]
+[Route("api/[controller]")]
+public class SampleController : ControllerBase
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-}
+    [HttpGet("public")]
+    public IActionResult Public() => Ok("Endpoint público!");
 
-app.Run();
+    [Authorize]
+    [HttpGet("protected")]
+    public IActionResult Protected()
+    {
+        var username = User.Identity?.Name ?? User.FindFirst("sub")?.Value;
+        return Ok(new
+        {
+            message = $"Bem-vindo, {username}!",
+            claims = User.Claims.Select(c => new { c.Type, c.Value })
+        });
+    }
+}
